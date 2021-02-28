d1c5f282801561a0601fff2e6d0b4058f4311d5b
Samtschan <33499618+Samtschan@users.noreply.github.com>
2021-02-28 13:00:30 +0000

    wishlist fix error

--- pokemon_cards/models.py
+++ pokemon_cards/models.py
@@ -46,4 +46,5 @@ class Wishlist(db.Model, BaseModelMixin):
     id = db.Column(db.String(255), primary_key=True)
     # TODO the ondelete is currently set to SET NULL just in case it breaks anything - please change to CASCADE later.
     user_id = db.Column(db.Integer(), db.ForeignKey("user.id", ondelete="SET NULL"), nullable=False)
-
+    # Relationships to other tables
+    user = relationship("User", back_populates="wishlists")
