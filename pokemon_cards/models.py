f10686bbe4ca5f958c698d3b41c1ee27908e99a3
Hadeel Mustafa <mustafa.hadeel18@gmail.com>
2021-03-01 17:33:12 +0000

    redefining relationships

--- pokemon_cards/models.py
+++ pokemon_cards/models.py
@@ -12,19 +12,20 @@ class BaseModelMixin(object):
 
 
 class Pokemon(db.Model, BaseModelMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(64), nullable=False)
 
 
 class Card(db.Model, BaseModelMixin):
-    id = db.Column(db.String(255), primary_key=True)
+    id = db.Column(db.String(255), primary_key=True, nullable=False)
     name = db.Column(db.String(127), nullable=False)
     supertype = db.Column(db.Enum(*SuperType.SuperTypes), nullable=False)
     subtype = db.Column(db.String(63))
+    # Relationships to other tables
 
 
 class User(db.Model, BaseModelMixin):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True, nullable=False)
     email = db.Column(db.String(127), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False, )
     bio = db.Column(db.String(255))
@@ -34,41 +35,45 @@ class User(db.Model, BaseModelMixin):
 
 
 class Deck(db.Model, BaseModelMixin):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True, nullable=False)
     # TODO the ondelete is currently set to SET NULL just in case it breaks anything - please change to CASCADE later.
-    user_id = db.Column(db.Integer(), db.ForeignKey(
-        "user.id", ondelete="SET NULL"), nullable=False)
-    description = db.Column(db.String(255), nullable=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id", ondelete="SET NULL"), nullable=False)
+    description = db.Column(db.String(255))
     # Relationships to other tables
-    user = relationship("User", back_populates="decks")
+    user = relationship("User", back_populates="decks", uselist=False)
+
+
+class CardDeck(db.Model, BaseModelMixin):
+    card_id = db.Column(db.String(255), db.ForeignKey("card.id"), primary_key=True, nullable=False)
+    deck_id = db.Column(db.Integer(), db.ForeignKey("deck.id", ondelete="CASCADE"), primary_key=True, nullable=False)
+    # Relationships to other tables
+    card = relationship("Card", backref="carddeck_card", foreign_keys=[card_id], uselist=False)
+    deck = relationship("Deck", backref="carddeck_deck", foreign_keys=[deck_id], uselist=False)
 
 
 class Wishlist(db.Model, BaseModelMixin):
     id = db.Column(db.String(255), primary_key=True)
-    # TODO the ondelete is currently set to SET NULL just in case it breaks anything - please change to CASCADE later.
-    user_id = db.Column(db.Integer(), db.ForeignKey(
-        "user.id", ondelete="SET NULL"), nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     # Relationships to other tables
     user = relationship("User", back_populates="wishlists")
 
 
-class Wishlist_cards(db.model, BaseModelMixin):
-    user_id = db.Column(db.Integer(), primary_key=True)
-    ForeignKey('Wishlist.user_id', ondelete="CASCADE")
-    card_id = db.Column(db.String(255), primary_key=True)
-    ForeignKey('Card.id', ondelete="NO ACTION")
+class WishlistCards(db.Model, BaseModelMixin):
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete="CASCADE"), primary_key=True,
+                        nullable=False)
+    card_id = db.Column(db.String(255), db.ForeignKey('card.id'), primary_key=True, nullable=False)
     threshold = db.Column(db.Float)
-    user = relationship('Wishlist', back_populates="Wishlist_cards")
-    Wishlist_cards = relationship('')
+    # Relationships to other tables
+    user = relationship('User', backref="wishlistcards_user", foreign_keys=[user_id], uselist=False)
+    card = relationship('Card', backref="wishlistcards_card", foreign_keys=[card_id], uselist=False)
 
 
-class Trades(db.model, BaseModelMixin):
-    id = db.Column(db.Integer(), primary_key=True),
-    seller_id = db.Column(db.Integer(), primary_key=False)
-    ForeignKey('user.seller_id', ondelete="NO ACTION")
-    purchaser_id = db.Column(db.Integer(), primary_key=False)
-    ForeignKey('user.purchaser_id', ondelete="NO ACTION")
-    card_id = db.Column(db.String(255), primary_key=False)
-    ForeignKey('cards.card_id', ondelete="NO ACTION")
-    user = relationship('User', back_populates="Trades")
-    Trades = relationship('Trades', back_populates="Cards")
+class Trades(db.Model, BaseModelMixin):
+    id = db.Column(db.Integer(), primary_key=True, nullable=False)
+    seller_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+    purchaser_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+    card_id = db.Column(db.String(255), db.ForeignKey('card.id'), nullable=False)
+    # Relationships to other tables
+    seller_user = relationship('User', backref="trades_seller", foreign_keys=[seller_id], uselist=False)
+    purchaser_user = relationship('User', backref="trades_purchaser", foreign_keys=[purchaser_id], uselist=False)
+    card = relationship('Card', backref="trades_card", foreign_keys=[card_id], uselist=False)
--- pokemon_cards/schemas.py
+++ pokemon_cards/schemas.py
@@ -1,7 +1,7 @@
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
-from pokemon_cards.models import Pokemon, User, Deck, Wishlist, Wishlist_cards, Trades
+from pokemon_cards.models import Pokemon, User, Deck, CardDeck, Wishlist, WishlistCards, Trades
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -25,20 +25,25 @@ class Deck(ma.SQLAlchemyAutoSchema):
         sqla_session = db.session
 
 
+class CardDeck(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = CardDeck
+        sqla_session = db.session
+
+
 class Wishlist(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Wishlist
         sqla_session = db.session
 
 
-class Wishlist_cards(ma.SQLAlchemyAutoSchema):
+class WishlistCards(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Wishlist_cards
+        model = WishlistCards
         sqla_session = db.session
 
 
 class Trades(ma.SQLAlchemyAutoSchema):
-
     class Meta:
         model = Trades
         sqla_session = db.session
